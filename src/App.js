1ed421ecc24f91febe1e9c043f37251f449dc7f3
sjh-00028 <sjh00028@mix.wvu.edu>
2024-02-25 19:05:15 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - 001</h2>
+      <p>WVU ID: 800391602</p>
+      <p>Hi I am Simon Hale</p>
+    </div>
+  );
+}
+
+export default App;
