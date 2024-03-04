1746dd1b376e28010f3691e511c9a7a2955ded80
sjh-00028 <sjh00028@mix.wvu.edu>
2024-03-03 21:25:03 -0500

    Week 8

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css"
+import Navbar from "./Navbar.js"
+import Card from "./Card.js"
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h1>CS 230L</h1>
       <h2>Section - 001</h2>
       <p>WVU ID: 800391602</p>
       <p>Hi I am Simon Hale</p>
+      <Card></Card>
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,26 @@
+function Card(props) {
+    return (
+        <div class="col">
+            <div class="card mb-4 rounded-3 shadow-sm">
+                <div class="card-header py-3">
+                    <h4 class="my-0 fw-normal">{props.title}</h4>
+                </div>
+                <div class="card-body">
+                    {props.children}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function Cards() {
+    return (
+        <div class="row">
+            <Card title="Card 1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Card>
+            <Card title="Card 2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Card>
+            <Card title="Card 3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Card>
+        </div>
+    );
+}
+
+export default Cards;
